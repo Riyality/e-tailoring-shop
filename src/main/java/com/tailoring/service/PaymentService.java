12a335ebdf7909ffc0b71d6a35180517a923d236
kolase1 <kolasesnehal.riyality@gmail.com>
2024-07-04 15:30:07 +0530

    task1

--- src/main/java/com/tailoring/service/PaymentService.java
+++ src/main/java/com/tailoring/service/PaymentService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 import com.tailoring.dao.PaymentDao;
 import com.tailoring.entity.Payment;
 
-@Service
-public class PaymentService {
+    @Service
+   public class PaymentService {
 
 	@Autowired
 	private PaymentDao paymentdao;
