fe798f70282758010008d05620f41e62f2925ab4
anusesachin <jut.amazon.acc@gmail.com>
2024-07-09 12:21:21 +0530

    code

--- src/main/java/com/tailoring/dao/PocketTypeDao.java
+++ src/main/java/com/tailoring/dao/PocketTypeDao.java
@@ -1,11 +1,15 @@
 package com.tailoring.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tailoring.entity.PocketType;
+
 @Repository
 public interface PocketTypeDao extends CrudRepository<PocketType, Integer> {
-	
-	
+
+	@Override
+	List<PocketType> findAll();
 }
